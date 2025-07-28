ec7f0030857f2bce947e1b71e18a4a885d775878
Aldo Román <hello@aldoroman.dev>
2025-07-29 00:01:40 +0200

    feat: Integrate with Resend

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -30,6 +30,20 @@ const config = {
     [APPROVAL_MODULE]: {
       resolve: "./modules/approval",
     },
+    [Modules.NOTIFICATION]: {
+      resolve: "@medusajs/medusa/notification",
+      providers: [
+        {
+          resolve: "./src/modules/resend",
+          id: "resend",
+          options: {
+            channels: ["email"],
+            api_key: process.env.RESEND_API_KEY,
+            from: process.env.RESEND_FROM_EMAIL,
+          },
+        },
+      ],
+    },
     [Modules.CACHE]: {
       resolve: "@medusajs/medusa/cache-inmemory",
     },
